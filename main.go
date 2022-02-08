46150d86ea8fb8466d4bdf2e171dde8c0219690c
Martin Sucha <git@mm.ms47.eu>
2022-02-08 20:56:31 +0100

    Check if sections exist
    We don't want links to nowhere.

--- main.go
+++ main.go
@@ -15,10 +15,11 @@ var templateFS embed.FS
 
 type TOCNode struct {
 	spec.TOCEntry
+	Exists   bool
 	Children []*TOCNode
 }
 
-func buildTOCTree(entries []spec.TOCEntry) []*TOCNode {
+func buildTOCTree(entries []spec.TOCEntry, sectionNumbers map[string]struct{}) []*TOCNode {
 	var root TOCNode
 	stack := []*TOCNode{&root}
 	for _, e := range entries {
@@ -27,9 +28,11 @@ func buildTOCTree(entries []spec.TOCEntry) []*TOCNode {
 			stack = stack[:level]
 		}
 		parent := stack[len(stack)-1]
+		_, exists := sectionNumbers[e.Number]
 		node := &TOCNode{
 			TOCEntry: e,
 			Children: nil,
+			Exists:   exists,
 		}
 		parent.Children = append(parent.Children, node)
 		stack = append(stack, node)
@@ -57,12 +60,16 @@ func link(sb *strings.Builder, href, text string) {
 	sb.WriteString(`</a>`)
 }
 
-func formatBody(text []spec.Text) template.HTML {
+func formatBody(text []spec.Text, sectionNumbers map[string]struct{}) template.HTML {
 	var sb strings.Builder
 	for _, t := range text {
 		switch {
 		case t.SectionRef != "":
-			link(&sb, "#s"+t.SectionRef, t.Text)
+			if _, ok := sectionNumbers[t.SectionRef]; ok {
+				link(&sb, "#s"+t.SectionRef, t.Text)
+			} else {
+				sb.WriteString(template.HTMLEscapeString(t.Text))
+			}
 		case t.Href != "":
 			link(&sb, t.Href, t.Text)
 		default:
@@ -72,14 +79,36 @@ func formatBody(text []spec.Text) template.HTML {
 	return template.HTML(sb.String())
 }
 
-func buildSections(in []spec.Section) []Section {
+func buildSections(in []spec.Section, sectionNumbers map[string]struct{}) []Section {
 	ret := make([]Section, len(in))
 	for i, s := range in {
 		ret[i].Section = in[i]
 		ret[i].Level = strings.Count(s.Number, ".") + 2
-		ret[i].BodyHTML = formatBody(in[i].Body)
+		ret[i].BodyHTML = formatBody(in[i].Body, sectionNumbers)
 	}
 	return ret
+}
+
+func checkSectionLinks(d spec.Document, sectionNumbers, tocNumbers map[string]struct{}) {
+
+	for _, t := range d.TOC {
+		if _, ok := sectionNumbers[t.Number]; !ok {
+			fmt.Fprintf(os.Stderr, "section %q exists in TOC, but not in sections\n", t.Number)
+		}
+	}
+	for _, s := range d.Sections {
+		if _, ok := tocNumbers[s.Number]; !ok {
+			fmt.Fprintf(os.Stderr, "section %q exists in sections, but not in TOC\n", s.Number)
+		}
+		for _, tt := range s.Body {
+			if tt.SectionRef != "" {
+				if _, ok := sectionNumbers[tt.SectionRef]; !ok {
+					fmt.Fprintf(os.Stderr, "non-existing section %q is referenced from section %q\n",
+						tt.SectionRef, s.Number)
+				}
+			}
+		}
+	}
 }
 
 func main() {
@@ -99,6 +128,15 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		return
 	}
+	sectionNumbers := make(map[string]struct{})
+	for _, s := range doc.Sections {
+		sectionNumbers[s.Number] = struct{}{}
+	}
+	tocNumbers := make(map[string]struct{})
+	for _, t := range doc.TOC {
+		tocNumbers[t.Number] = struct{}{}
+	}
+	checkSectionLinks(doc, sectionNumbers, tocNumbers)
 	tmpl, err := template.ParseFS(templateFS, "template.gohtml")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
@@ -111,8 +149,8 @@ func main() {
 	}
 	err = tmpl.Execute(f, templateData{
 		Document: doc,
-		TOCTree:  buildTOCTree(doc.TOC),
-		Sections: buildSections(doc.Sections),
+		TOCTree:  buildTOCTree(doc.TOC, sectionNumbers),
+		Sections: buildSections(doc.Sections, sectionNumbers),
 	})
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
